6c79133975d3d94ed7f302a89eb4e2a250988571
joeldemars <joeltdemars@gmail.com>
2025-02-27 14:42:53 -0700

    Move generateCommand into command.ts

--- src/command/command.ts
+++ src/command/command.ts
@@ -14,3 +14,55 @@ export interface Command {
 export type Container = "mp4" | "webm" | "mkv";
 export type VideoCodec = "avc" | "hevc" | "vp9" | "av1";
 export type AudioCodec = "aac" | "opus" | "wav" | "flac";
+
+export function generateCommand(infile: string, container: string, vcodec: string, acodec: string, outfile: string): string {
+    let command = "ffmpeg -i ";
+
+    if (infile == "") {
+        command += "<input file> ";
+    } else if (/ /.test(infile)) {
+        command += "'" + infile + "' ";
+    } else {
+        command += infile + " ";
+    }
+
+    switch (vcodec) {
+        case "avc":
+            command += "-c:v libx264 ";
+            break;
+        case "hevc":
+            command += "-c:v libx265 ";
+            break;
+        case "vp9":
+            command += "-c:v libvpx-vp9 ";
+            break;
+        case "av1":
+            command += "-c:v libaom-av1 ";
+            break;
+    }
+
+    switch (acodec) {
+        case "aac":
+            command += "-c:a aac ";
+            break;
+        case "opus":
+            command += "-c:a libopus ";
+            break;
+        case "wav":
+            command += "-c:a pcm_s16le ";
+            break;
+        case "flac":
+            command += "-c:a flac ";
+            break;
+    }
+
+    if (outfile == "") {
+        command += "<output file>." + container;
+    } else if (/ /.test(outfile)) {
+        command += "'" + outfile + "." + container + "'";
+    } else {
+        command += outfile + "." + container;
+    }
+
+    return command;
+}
--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Command, Container, AudioCodec, VideoCodec } from '../command/command';
+import { Command, Container, AudioCodec, VideoCodec, generateCommand } from '../command/command';
 import './home.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
@@ -106,55 +106,3 @@ const Home: React.FC<HomeProps> = ({user}) => {
 };
 
 export default Home;
-
-function generateCommand(infile: string, container: string, vcodec: string, acodec: string, outfile: string): string {
-    let command = "ffmpeg -i ";
-
-    if (infile == "") {
-        command += "<input file> ";
-    } else if (/ /.test(infile)) {
-        command += "'" + infile + "' ";
-    } else {
-        command += infile + " ";
-    }
-
-    switch (vcodec) {
-        case "avc":
-            command += "-c:v libx264 ";
-            break;
-        case "hevc":
-            command += "-c:v libx265 ";
-            break;
-        case "vp9":
-            command += "-c:v libvpx-vp9 ";
-            break;
-        case "av1":
-            command += "-c:v libaom-av1 ";
-            break;
-    }
-
-    switch (acodec) {
-        case "aac":
-            command += "-c:a aac ";
-            break;
-        case "opus":
-            command += "-c:a libopus ";
-            break;
-        case "wav":
-            command += "-c:a pcm_s16le ";
-            break;
-        case "flac":
-            command += "-c:a flac ";
-            break;
-    }
-
-    if (outfile == "") {
-        command += "<output file>." + container;
-    } else if (/ /.test(outfile)) {
-        command += "'" + outfile + "." + container + "'";
-    } else {
-        command += outfile + "." + container;
-    }
-
-    return command;
-}
